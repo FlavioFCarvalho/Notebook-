efe103cf6b7d7ac3cd36a6011765e6058284bc49
Flavio Carvalho <flavio@globalnetsis.com.br>
2016-10-03 17:30:37 -0300

    Inibindo a rota edit caso precise

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :phones
   resources :addresses
-  resources :contacts
+  resources :contacts #, except: [:edit]
   resources :kinds
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
