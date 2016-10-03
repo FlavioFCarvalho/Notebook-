acc7dafe4332cc113fde664689aa5008686a013c
Flavio Carvalho <flavio@globalnetsis.com.br>
2016-10-03 10:21:05 -0300

    Open house com os scaffolds Address, Contact, Kind e Phone

---
+++ db/schema.rb
@@ -0,0 +1,56 @@
+# encoding: UTF-8
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 20161003121713) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "street",     limit: 255
+    t.string   "city",       limit: 255
+    t.string   "state",      limit: 255
+    t.integer  "contact_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "addresses", ["contact_id"], name: "index_addresses_on_contact_id", using: :btree
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.string   "email",      limit: 255
+    t.integer  "kind_id",    limit: 4
+    t.text     "rmk",        limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "contacts", ["kind_id"], name: "index_contacts_on_kind_id", using: :btree
+
+  create_table "kinds", force: :cascade do |t|
+    t.string   "description", limit: 255
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
+
+  create_table "phones", force: :cascade do |t|
+    t.string   "phone",      limit: 255
+    t.integer  "contact_id", limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "phones", ["contact_id"], name: "index_phones_on_contact_id", using: :btree
+
+  add_foreign_key "addresses", "contacts"
+  add_foreign_key "contacts", "kinds"
+  add_foreign_key "phones", "contacts"
+end
